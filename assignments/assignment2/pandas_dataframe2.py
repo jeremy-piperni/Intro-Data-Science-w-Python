6cb70fa2c9518aa09fb0ca9569ea3431e704f7c2
coo1isj <coo1piperni@gmail.com>
2022-07-14 01:06:50 -0400

    Made correction in pandas_dataframe2.py

--- assignments/assignment2/pandas_dataframe2.py
+++ assignments/assignment2/pandas_dataframe2.py
@@ -5,12 +5,13 @@ def average_influenze_doses():
 
     df = pd.read_csv('D://Data Science/Introduction to Data Science/assignments/assignment2/assets/NISPUF17.csv')
     #df = pd.read_csv('assets/NISPUF17.csv')
+    df = df.dropna(subset=['P_NUMFLU'])
 
     breastfed_df = df[df['CBF_01'] == 1]
     breastfed_influenza = breastfed_df['P_NUMFLU']
     total = np.sum(breastfed_influenza)/len(breastfed_influenza)
 
-    non_breastfed_df = df[df['CBF_01'] != 1]
+    non_breastfed_df = df[df['CBF_01'] == 2]
     non_breastfed_influenza = non_breastfed_df['P_NUMFLU']
     total2 = np.sum(non_breastfed_influenza)/len(non_breastfed_influenza)
 
